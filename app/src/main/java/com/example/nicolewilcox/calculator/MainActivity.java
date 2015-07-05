ae5d3b559c1b4a0a1e7ab1407fe73b4f36019e63
Nicol Wilcox <nwilcoxnz@gmail.com>
2015-07-05 20:54:11 +1200

    Minor fixes and change of colour scheme. Still has big issues which need to be fixed.

--- app/src/main/java/com/example/nicolewilcox/calculator/MainActivity.java
+++ app/src/main/java/com/example/nicolewilcox/calculator/MainActivity.java
@@ -21,31 +21,32 @@ public class MainActivity extends ActionBarActivity {
 
     ArrayList <String> arrayList = new ArrayList<String>();
 
-    String string = "";
-    String string2 = "";
+    String calculationString = ""; // To hold what is to be displayed in the calculation (upper) field
+    String string2 = ""; // To hold what has just been entered, and will be added to string
     int total = 0; // Result of the calculation(s)
 
     public void onClick (View v){ // What happens when you click buttons 0 - 9, "+', "-", "/" or "*"
 
         TextView textResult = (TextView) findViewById(R.id.textCalculation); // Calculation field
         Button button = (Button) v;
-        string = (String) button.getText().toString();
+        calculationString = (String) button.getText().toString();
 
-        if (!string.contains("+") && !string.contains("-") && !string.contains("/") && !string.contains("*")){
-            string2 = string2 + string;
-            if (arrayList.size() > 0){ // Remove the value that was last entered
+        if (!calculationString.contains("+") && !calculationString.contains("-") && !calculationString.contains("/") && !calculationString.contains("*")){
+            string2 = string2 + calculationString;
+            if (!arrayList.isEmpty()){ // Remove the value that was last entered
                 arrayList.remove(arrayList.size()-1);
             }
             arrayList.add(string2);
         }
 
         else {
-            arrayList.add(string);
-            arrayList.add(string);
+            arrayList.add(calculationString);
+            arrayList.add(calculationString);
             string2 = ""; // Clear the calculation string
         }
 
-        textResult.setText(textResult.getText().toString() + string); // Display the result text
+        textResult.setText(textResult.getText().toString() + calculationString); // Display the result text
+
     }
 
     public void onClickEquals (View v){ // What happens when you press "="
@@ -54,115 +55,130 @@ public class MainActivity extends ActionBarActivity {
         TextView textError = (TextView) findViewById(R.id.textError); // Error field
         Button button = (Button) v;
         String string = (String) button.getText().toString();
-        boolean isValid = false; // Check if the calculation has a valid structure
+        boolean isValid = false; // True if the calculation has a valid structure
 
         int numLoops = arrayList.size();
+        System.out.println("ARRAY SIZE: " + arrayList.size());
+        System.out.println("NUM LOOPS: " + numLoops);
 
-        // Check that the syntactic structure of the expression is correct
+        total = 0; // Reset total
 
-        for (int i = 0; i < arrayList.size()-1; i++){
-            if (arrayList.isEmpty()) { // If a calculation hasn't been entered
-                textResult.setText(total);
-                break;
-            }
-            if (arrayList.get(i).equals("+") || arrayList.get(i).equals("-") || arrayList.get(i).equals("/") || arrayList.get(i).equals("*")){// If we are looking at a symbol then the next character must not be a symbol
-                if (arrayList.get(i+1).equals("+") || arrayList.get(i+1).equals("-") || arrayList.get(i+1).equals("/") || arrayList.get(i+1).equals("*")){
-                    textError.setText("Invalid operation");
-                    break;
-                }
-            }
-            isValid = true;
-        }
+        if (!arrayList.isEmpty()) { // If a calculation has been entered
 
-        while (numLoops != 1 && isValid == true){
-
-            if (arrayList.isEmpty()) { // If a calculation hasn't been entered
-                break;
-            }
-
-//            if (!arrayList.isEmpty() && numLoops == 2) { // If a calculation hasn't been entered
-//                break;
-//            }
-
-            if (numLoops > 3){ // If you are doing more than one calculation
-
-                if (arrayList.get(3).contains("*") || arrayList.get(3).contains("/")){ // Calculate multiplication and division in the correct order by evaluating the second calculation
-
-                    arrayList.remove(4); // Remove values that have been calculated
-                    arrayList.remove(3); // Remove values that have been calculated
-                    arrayList.remove(2); // Remove values that have been calculated
-
-                    arrayList.add(2, Integer.toString(total));
-                    numLoops = arrayList.size(); // Re-calculate array size/number of loops
-                }
-
-                else { // Evaluate the first calculation
-                    if (arrayList.get(1).contains("+")){ total = (Integer.parseInt(arrayList.get(0))) + ((Integer.parseInt(arrayList.get(2)))); // Add the first and second digits
-                    }
-                    else if (arrayList.get(1).contains("-")){ total = (Integer.parseInt(arrayList.get(0))) - (Integer.parseInt(arrayList.get(2))); // Subtract the first and second digits
-                    }
-                    else if (arrayList.get(1).contains("*")){ total = (Integer.parseInt(arrayList.get(0))) * ((Integer.parseInt(arrayList.get(2)))); // Multiply the first and second digits
-                    }
-                    else if (arrayList.get(1).contains("/")){
-                        if (Integer.parseInt(arrayList.get(2)) == 0) { // Don't allow dividing by 0
-                            textError.setText("Invalid operation");
-                            break;
-                        }
-                        total = (Integer.parseInt(arrayList.get(0))) / (Integer.parseInt(arrayList.get(2))); // Divide the first and second digits
-                    }
-
-                    arrayList.remove(2); // Remove values that have been calculated
-                    arrayList.remove(1); // Remove values that have been calculated
-                    arrayList.remove(0); // Remove values that have been calculated
-
-                    arrayList.add(0, Integer.toString(total));
-                    numLoops = arrayList.size(); // Re-calculate array size/number of loops
-                }
-            }
-
-            else if (numLoops == 3){ // If size of arrayList is 3 then you have one calculation
-                if (arrayList.get(1).contains("+")){ total = Integer.parseInt(arrayList.get(0)) + (Integer.parseInt(arrayList.get(2))); // Add the first and second digits
-                }
-                else if (arrayList.get(1).contains("-")){ total = Integer.parseInt(arrayList.get(0)) - (Integer.parseInt(arrayList.get(2))); // Subtract the first and second digits
-                }
-                else if (arrayList.get(1).contains("*")){ total = Integer.parseInt(arrayList.get(0)) * (Integer.parseInt(arrayList.get(2))); // Multiply the first and second digits
-                }
-                else if (arrayList.get(1).contains("/")){
-                    if (Integer.parseInt(arrayList.get(2)) == 0) { // Don't allow dividing by 0
+            for (int i = 0; i < arrayList.size(); i++) { // Check that the syntactic structure of the expression is correct
+                if (arrayList.get(i).equals("+") || arrayList.get(i).equals("-") || arrayList.get(i).equals("/") || arrayList.get(i).equals("*")) {// If we are looking at a symbol then the next character must not be a symbol
+                    if (arrayList.get(i + 1).equals("+") || arrayList.get(i + 1).equals("-") || arrayList.get(i + 1).equals("/") || arrayList.get(i + 1).equals("*")) {
                         textError.setText("Invalid operation");
                         break;
                     }
-                    total = Integer.parseInt(arrayList.get(0)) / (Integer.parseInt(arrayList.get(2))); // Divide the first and second digits
                 }
+                isValid = true;
+            }
 
-                arrayList.remove(2); // Remove values that have been calculated
-                arrayList.remove(1); // Remove values that have been calculated
-                arrayList.remove(0); // Remove values that have been calculated
-
-                arrayList.add(0, Integer.toString(total));
-                numLoops = arrayList.size(); // Re-calculate array size/number of loops
+            if (numLoops == 1) { // If you've only entered one number or symbol
+                // Make sure the data entered is a number
+                if (arrayList.get(0).equals("+") || arrayList.get(0).equals("-") || arrayList.get(0).equals("/") || arrayList.get(0).equals("*")) {
+                    textError.setText("Invalid operation");
+                } else {
+                    total = Integer.parseInt(arrayList.get(0));
+                }
+                textResult.setText(Integer.toString(total)); // Display the calculation
             }
             else {
-                textError.setText("Invalid operation");
-                break;
+                while (numLoops != 1 && isValid == true) {
+
+                    if (numLoops == 2) { // If the user enters only one number and one operand (in that order)
+                        if (arrayList.get(1).equals("+") || arrayList.get(1).equals("-") || arrayList.get(1).equals("/") || arrayList.get(1).equals("*")) {// If we are looking at a symbol then the next character must not be a symbol
+                            System.out.println("WFUBRADBVEFJNVJDFNBJNDFJBNFJDV_))))000000000000000");
+                            textError.setText("Invalid operation");
+                            break;
+                        }
+                    }
+
+                    if (numLoops > 3) { // If you are doing more than one calculation
+
+                        if (arrayList.get(3).contains("*") || arrayList.get(3).contains("/")) { // Calculate multiplication and division in the correct order by evaluating the second calculation
+
+                            arrayList.remove(4); // Remove values that have been calculated
+                            arrayList.remove(3); // Remove values that have been calculated
+                            arrayList.remove(2); // Remove values that have been calculated
+
+                            arrayList.add(2, Integer.toString(total));
+                            numLoops = arrayList.size(); // Re-calculate array size/number of loops
+                        } else { // Evaluate the first calculation
+                            if (arrayList.get(1).contains("+")) {
+                                total = (Integer.parseInt(arrayList.get(0))) + ((Integer.parseInt(arrayList.get(2)))); // Add the first and second digits
+                            } else if (arrayList.get(1).contains("-")) {
+                                total = (Integer.parseInt(arrayList.get(0))) - (Integer.parseInt(arrayList.get(2))); // Subtract the first and second digits
+                            } else if (arrayList.get(1).contains("*")) {
+                                total = (Integer.parseInt(arrayList.get(0))) * ((Integer.parseInt(arrayList.get(2)))); // Multiply the first and second digits
+                            } else if (arrayList.get(1).contains("/")) {
+                                if (Integer.parseInt(arrayList.get(2)) == 0) { // Don't allow dividing by 0
+                                    textError.setText("Invalid operation");
+                                    break;
+                                }
+                                total = (Integer.parseInt(arrayList.get(0))) / (Integer.parseInt(arrayList.get(2))); // Divide the first and second digits
+                            }
+
+                            arrayList.remove(2); // Remove values that have been calculated
+                            arrayList.remove(1); // Remove values that have been calculated
+                            arrayList.remove(0); // Remove values that have been calculated
+
+                            arrayList.add(0, Integer.toString(total));
+                            numLoops = arrayList.size(); // Re-calculate array size/number of loops
+                        }
+                    } else if (numLoops == 3) { // If size of arrayList is 3 then you have one calculation
+                        if (arrayList.get(1).contains("+")) {
+                            total = Integer.parseInt(arrayList.get(0)) + (Integer.parseInt(arrayList.get(2))); // Add the first and second digits
+                        } else if (arrayList.get(1).contains("-")) {
+                            total = Integer.parseInt(arrayList.get(0)) - (Integer.parseInt(arrayList.get(2))); // Subtract the first and second digits
+                        } else if (arrayList.get(1).contains("*")) {
+                            total = Integer.parseInt(arrayList.get(0)) * (Integer.parseInt(arrayList.get(2))); // Multiply the first and second digits
+                        } else if (arrayList.get(1).contains("/")) {
+                            if (Integer.parseInt(arrayList.get(2)) == 0) { // Don't allow dividing by 0
+                                textError.setText("Invalid operation");
+                                break;
+                            }
+                            total = Integer.parseInt(arrayList.get(0)) / (Integer.parseInt(arrayList.get(2))); // Divide the first and second digits
+                        }
+
+                        arrayList.remove(2); // Remove values that have been calculated
+                        arrayList.remove(1); // Remove values that have been calculated
+                        arrayList.remove(0); // Remove values that have been calculated
+
+                        arrayList.add(0, Integer.toString(total));
+                        numLoops = arrayList.size(); // Re-calculate array size/number of loops
+                    } else {
+                        textError.setText("Invalid operation");
+                        break;
+                    }
+                }
+                System.out.println("total: " + total);
+
+                textResult.setText(Integer.toString(total)); // Display the calculation
+                total = 0;
+                string2 = "";
+//        textResult.setText(arrayList.toString());
+
+                int i = arrayList.size();
+                while (i > 0) { // Remove everything from arrayList
+                    arrayList.remove(i - 1); // Account for indexing from 0
+                    i--;
+                }
             }
         }
-
-        textResult.setText(Integer.toString(total)); // Display the calculation
-//        textResult.setText(arrayList.toString());
     }
 
     public void onClickCE (View v){
         TextView textResult = (TextView) findViewById(R.id.textResult); // Result field
         TextView textCalculation = (TextView) findViewById(R.id.textCalculation); // Calculation field
         TextView textError = (TextView) findViewById(R.id.textError); // Error field
-        //        arrayList.removeAll();
-        string = "";
-        string2 = "";
 
         textResult.setText("0");
         textCalculation.setText("");
         textError.setText("");
+
+        total = 0; // Reset the current calculation
 
         if (!arrayList.isEmpty()) {
             int i = arrayList.size();
@@ -173,7 +189,6 @@ public class MainActivity extends ActionBarActivity {
                 i--;
             }
         }
-
     }
 
     void singleCalculation(int total, int numLoops){
